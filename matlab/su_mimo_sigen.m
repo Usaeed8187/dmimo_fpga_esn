de17b0e486233fcc460dbf3a2f4102cdcef4b912
Donald Liang <yiliang@vt.edu>
2025-03-04 16:35:08 -0500

    Add SU-MIMO test files
    * Add SU-MIMO signal generation Matlab scripts
    * Update GRC test files

--- matlab/su_mimo_sigen.m
+++ matlab/su_mimo_sigen.m
@@ -1,7 +1,6 @@
-function ncjt_sc_sigen(psdulen, modtype, cctype, datadir)
+function su_mimo_sigen(mimotype, psdulen, modtype, cctype, datadir)
 
 % System configuration
-mimotype = '1t1s';
 cfg = sys_config(mimotype, psdulen, modtype, cctype);
 
 % Create data output folder
@@ -20,17 +19,12 @@ txPSDU = randi([0 1], psdulen*8, 1, 'int8'); % PSDULength in bytes
 % Generate data symbols
 [txdata, encdata, strmdata, txdsyms] = tx_processing(cfg, txPSDU);
 
-% Dummy STBC transmitter
-txdata = 1/sqrt(2) * [txdata(:), txdata(:)];
-
 % Time-domain preamble signals (HT-SIG, HT-STF, HT-LTF)
-htltfx = cat(1, [b.htltf(1:80,1), zeros(80,1)], ...
-                [zeros(80,1), b.htltf(1:80,1)]);
-preamble = [b.lstf; b.lltf; b.lsig; b.htsig; b.htstf; htltfx];
+preamble = [b.lstf; b.lltf; b.lsig; b.htsig; b.htstf; b.htltf];
 
 % Prepare transmitter signal for USRP
-txFrame = reshape([preamble; txdata], (cfg.Nfft+cfg.Ncp), [], 2);
-txFrame = reshape(txFrame, [], 2);
+txFrame = reshape([preamble; txdata], (cfg.Nfft+cfg.Ncp), [], cfg.Nt);
+txFrame = reshape(txFrame, [], cfg.Nt);
 
 % Convert Tx signals to range (-1,1)
 % scaling = 1.0/max(abs([real(txFrame(:)); imag(txFrame(:))]));
@@ -38,7 +32,7 @@ scaling = 0.5; % fixed scaling
 txsig = signal_clipping(scaling * txFrame);
 
 % Zero-padding for burst transmission
-padding = zeros(1000, 2);
+padding = zeros(1000, cfg.Nt);
 txsig = [padding; txsig; padding];
 
 % Show system parameters
@@ -55,8 +49,10 @@ fprintf("Tx signal scaling: %.15f\n", scaling);
 fprintf("Saving GNU Radio Tx data files ...\n")
 write_complex_binary(txsig(:,1), ...
     sprintf('%s/%s/%s/txsig_s1.bin',datadir,mimotype,modtype));
+write_complex_binary(txsig(:,2), ...
+    sprintf('%s/%s/%s/txsig_s2.bin',datadir,mimotype,modtype));
 write_complex_binary(scaling*preamble, ...
-    sprintf('%s/%s/%s/ncjt_preamble.bin',datadir,mimotype,modtype));
+    sprintf('%s/%s/%s/preamble.bin',datadir,mimotype,modtype));
 
 fid = fopen(sprintf('%s/%s/%s/enc_data.bin',datadir,mimotype,modtype),'wb');
 fwrite(fid, encdata(:), "char");
@@ -66,10 +62,19 @@ fid = fopen(sprintf('%s/%s/%s/strm_data.bin',datadir,mimotype,modtype),'wb');
 fwrite(fid, strmdata(:), "char");
 fclose(fid);
 
+fid = fopen(sprintf('%s/%s/%s/tx_strm_data.bin',datadir,mimotype,modtype),'wb');
+txstrmdata = strmdata.';
+fwrite(fid, txstrmdata, "char");
+fclose(fid);
+
 txdsyms = txdsyms(cfg.scInd, :, :);
-txdsyms_tmp = reshape(permute(txdsyms, [3, 1, 2]), [], 1);
-write_complex_binary(txdsyms_tmp, ...
+txdsyms_intlv = reshape(permute(txdsyms, [3, 1, 2]), [], 1); % (Nss, Nsc, Nsyms)
+write_complex_binary(txdsyms_intlv, ...
     sprintf("%s/%s/%s/txdsyms.bin",datadir,mimotype,modtype));
+write_complex_binary(txdsyms(:,:,1), ...
+    sprintf("%s/%s/%s/txdsyms_s1.bin",datadir,mimotype,modtype));
+write_complex_binary(txdsyms(:,:,2), ...
+    sprintf("%s/%s/%s/txdsyms_s2.bin",datadir,mimotype,modtype));
 
 fprintf("\n")
 
